ab8b443dac8d92a11da753b0c7ff521cf1c8be0c
philippethiran <pthiran@gmail.com>
2023-05-23 07:45:52 +0200

    Exercises: oo multiple inheritance

--- exercises/oo/python_multipleheritance.py
+++ exercises/oo/python_multipleheritance.py
@@ -2,9 +2,11 @@ class Mammal:
     def mammal_info(self):
         print("Mammals can give direct birth.")
 
+
 class WingedAnimal:
     def winged_animal_info(self):
         print("Winged animals can flap.")
+
 
 class Bat(Mammal, WingedAnimal):
     pass
